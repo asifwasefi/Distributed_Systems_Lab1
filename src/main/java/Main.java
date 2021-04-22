778fe2dd6e2917c3d62ea8d3ce27f703c91812c4
asifwasefi <asifwasefi@yahoo.com>
2021-04-23 01:19:26 +0200

    Initial commit
    Hello world

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,8 +4,6 @@
  * Created by asif on 23/04/2021
  */
 public class Main {
-
-
     public static void main(String[] args) {
         System.out.println("Hello World!"); // Display the string.
     }
