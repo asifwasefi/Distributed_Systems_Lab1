bb9acc663b1849d46a76e597ea128d52a931dc8c
asifwasefi <asifwasefi@yahoo.com>
2021-04-23 01:07:49 +0200

    Hello_world
    Only hello world code written

---
+++ src/main/java/Main.java
@@ -0,0 +1,14 @@
+
+
+/**
+ * Created by asif on 23/04/2021
+ */
+public class Main {
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!"); // Display the string.
+    }
+
+
+}
