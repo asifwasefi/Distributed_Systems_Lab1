7bf91a63e00f2f97c912f75b6c052d263ab74a36
asifwasefi <asifwasefi@yahoo.com>
2021-04-23 02:07:12 +0200

    Main changed after merge

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,7 +5,7 @@
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World! main branch"); // Display the string.
+        System.out.println("Hello World!"); // Display the string.
     }
 
 
